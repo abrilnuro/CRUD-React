400cd12212be4967aebc9823d48125d295b0d376
abril <13170789@itculiacan.edu.mx>
2020-03-24 13:07:55 -0700

    se obtienen la informacion de los inputs y se hizo la accion del boton submit

--- src/components/create.component.js
+++ src/components/create.component.js
@@ -1,6 +1,38 @@
 import React from 'react';
 
 class Create extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            personName: '',
+            businessName: '',
+            businessGstNumber: ''
+        }
+
+        this.onChangePersonName = this.onChangePersonName.bind(this);
+        this.onChangeBusinessName = this.onChangeBusinessName.bind(this);
+        this.onChangeGstNumber = this.onChangeGstNumber.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+
+    onChangePersonName(e) {
+        this.setState({personName: e.target.value});
+    }
+
+    onChangeBusinessName(e) {
+        this.setState({businessName: e.target.value});
+    }
+
+    onChangeGstNumber(e) {
+        this.setState({businessGstNumber: e.target.value});
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+        alert(this.state.businessGstNumber)
+    }
+
     render() {
         return(
             <React.Fragment>
@@ -10,16 +42,27 @@ class Create extends React.Component {
                     <form>
                         <div className="form-group">
                             <label>Add Person Name:</label>
-                            <input type="text" className="form-control"></input>
+                            <input type="text" className="form-control" 
+                            value={this.state.personName}
+                            onChange={this.onChangePersonName}/>
+                        </div>
+                        <div className="form-group">
+                            <label>Business Name: </label>
+                            <input type="text" className="form-control"
+                            value={this.state.businessName}
+                            onChange={this.onChangeBusinessName}/>
                         </div>
 
                         <div className="form-group">
                             <label>Add GST Number: </label>
-                            <input type="text" className="form-control"/>
+                            <input type="text" className="form-control"
+                            vaue={this.state.businessGstNumber}
+                            onChange={this.onChangeGstNumber}/>
                         </div>
 
                         <div className="form-group">
-                            <input type="submit" value="Register Business" className="btn btn-primary"/>
+                            <input type="submit" value="Register Business" 
+                            className="btn btn-primary" onClick={this.onSubmit}/>
                         </div>
                     </form>
                 </div>
