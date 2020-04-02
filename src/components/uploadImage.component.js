99afb51e291023fbbe8740e30fbe8e16d693bf4f
abril <13170789@itculiacan.edu.mx>
2020-04-02 14:46:13 -0700

    se consume servicio para guardar imagen

--- src/components/uploadImage.component.js
+++ src/components/uploadImage.component.js
@@ -1,30 +1,73 @@
 import React from 'react';
-import Input from '@material-ui/core/Input';
+import Axios from 'axios';
 
 class Upload extends React.Component {
   constructor(props){
     super(props)
     this.state = {
-      file: null
+      file: null,
+      temporalFile: null,
+      photo: null
     }
     this.handleChange = this.handleChange.bind(this)
   }
 
-  handleChange(event) {
+  componentDidMount(){
+    const id = "5e865948f1e7d063000cf9cc";
+    Axios.get('http://localhost:8080/api/photos'+ id)
+      .then(response => {
+        this.setState({ 
+          temporalFile: URL.createObjectURL(response.data) 
+        });
+      }).catch(function (error) {
+        console.log(error);
+      });
+  }
+
+  handleChange = (e) => {
+    e.preventDefault();
     this.setState({
-      file: URL.createObjectURL(event.target.files[0])
-    })
+      file: e.target.files[0],
+      temporalFile: URL.createObjectURL(e.target.files[0]),
+    });
+  }
+
+  submit = () => {
+    const formData = new FormData();
+    formData.append('file', this.state.file);
+
+    Axios.post('http://localhost:8080/api/upload', formData, {
+      headers: {'Content-Type': 'multipart/form-data' }
+    }).then((response) => {
+      console.log(response.data);
+    }).catch((error) => {
+      console.log(error);
+    });
   }
 
   render() {
     return (
-      <div className="border border-dark">
-        <img style={{width: 200, height: 200}}
-            src={this.state.file}/>
+      <React.Fragment>
+        <div className="container">
 
-        <Input type="file" onChange={this.handleChange}/>
+          <img style={{width: 200, height: 150}}
+            className="border border-dark"
+            src={this.state.temporalFile}/>
 
-      </div>
+          <img src={"data:image/png;base64," + this.state.photo} />
+
+        
+            <div className="row">
+                <div className="col-12">
+                        <div className="form-group files color">
+                            <input type="file" className="form-control" 
+                              name="file" onChange={this.handleChange}/>
+                        </div>
+                </div>
+            </div>
+        </div>
+        <button onClick={this.submit}>submit</button>
+      </React.Fragment>
     );
   }
 }
